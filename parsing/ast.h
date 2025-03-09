25a03349d2e795d1599bf55c897bf07951025a56
Kody-Schram <kody.schram@icloud.com>
2025-03-09 11:24:50 -0400

    created types for ast in parserTypes

--- parsing/ast.h
+++ parsing/ast.h
@@ -1,20 +1,4 @@
 #ifndef AST_H
 #define AST_H
 
-typedef enum Node_Type {
-    NODE_NUMBER,
-    NODE_OPERATOR,
-    NODE_VARIABLE,
-    NODE_FUNC_CALL,
-    NODE_FUNC_DEF
-} NodeType;
-
-typedef struct ASTNode {
-    NodeType type;
-    char *value;
-
-    ASTNode *left;
-    ASTNode *right;
-} ASTNode;
-
 #endif
--- parsing/parserTypes.h
+++ parsing/parserTypes.h
@@ -18,4 +18,21 @@ typedef struct Token {
     struct Token* next;
 } Token;
 
+
+typedef enum NodeType {
+    NODE_NUMBER,
+    NODE_OPERATOR,
+    NODE_VARIABLE,
+    NODE_FUNC_CALL,
+    NODE_FUNC_DEF
+} NodeType;
+
+typedef struct ASTNode {
+    NodeType type;
+    char *value;
+
+    ASTNode *left;
+    ASTNode *right;
+} ASTNode;
+
 #endif
