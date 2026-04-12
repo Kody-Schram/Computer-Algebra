2e45cbc55d43e5a713e8c813623e9bb959868178
Kody Schram <kody.schram@icloud.com>
2026-04-11 18:21:58 -0700

    Add definitions for axiom operations

---
+++ src/core/axioms.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "axioms.h"
+#include "core/utils/context/context.h"
+#include "core/utils/context/environment.h"
+#include "core/utils/log.h"
+#include "core/utils/types.h"
+
+
+static Function *createOpFunc(Expression *(*func) (Expression **args, int nArgs)) {
+    Function *op = malloc(sizeof(Function));
+    if (op == nullptr) return nullptr;
+    op->type = BUILTIN;
+    op->parameters = 2;
+    op->builtin = func;
+    op->env = createEnvironment(ENV_LIST);
+    
+    Expression *a = dummyExpression(EXPRESSION_DOUBLE);
+    Expression *b = dummyExpression(EXPRESSION_DOUBLE);
+    if (a == nullptr || b == nullptr) return nullptr;
+    
+    a->value = 0;
+    b->value = 0;
+    if (!bindComponent(op->env, COMP_VARIABLE, "a", a) || !bindComponent(op->env, COMP_VARIABLE, "b", b)) return nullptr;
+    
+    return op;
+}
+
+
+Expression *add(Expression **args, int nArgs) {
+    if (nArgs != 2) return nullptr;
+    
+    // If not both numbers, return the addition expression again
+    if (args[0]->type != EXPRESSION_INTEGER && args[0]->type != EXPRESSION_DOUBLE && 
+        args[1]->type != EXPRESSION_INTEGER && args[1]->type != EXPRESSION_DOUBLE) return nullptr;
+    
+    if (args[0]->type == EXPRESSION_INTEGER && args[1]->type == EXPRESSION_INTEGER) {
+        Expression *result = dummyExpression(EXPRESSION_INTEGER);
+        if (result == nullptr) return nullptr;
+        
+        result->integer = args[0]->integer + args[1]->integer;
+        return result;
+    }
+    
+    double a = (args[0]->type == EXPRESSION_INTEGER) ? (double) args[0]->integer : args[0]->value;
+    double b = (args[1]->type == EXPRESSION_INTEGER) ? (double) args[1]->integer : args[1]->value;
+    
+    Expression *result = dummyExpression(EXPRESSION_DOUBLE);
+    if (result == nullptr) return nullptr;
+    result->value = a + b;
+    
+    return result;
+}
+
+
+Expression *multiply(Expression **args, int nArgs) {
+    if (nArgs != 2) return nullptr;
+    
+    // If not both numbers, return the addition expression again
+    if (args[0]->type != EXPRESSION_INTEGER && args[0]->type != EXPRESSION_DOUBLE && 
+        args[1]->type != EXPRESSION_INTEGER && args[1]->type != EXPRESSION_DOUBLE) return nullptr;
+    
+    if (args[0]->type == EXPRESSION_INTEGER && args[1]->type == EXPRESSION_INTEGER) {
+        Expression *result = dummyExpression(EXPRESSION_INTEGER);
+        if (result == nullptr) return nullptr;
+        
+        result->integer = args[0]->integer * args[1]->integer;
+        return result;
+    }
+    
+    double a = (args[0]->type == EXPRESSION_INTEGER) ? (double) args[0]->integer : args[0]->value;
+    double b = (args[1]->type == EXPRESSION_INTEGER) ? (double) args[1]->integer : args[1]->value;
+    
+    Expression *result = dummyExpression(EXPRESSION_DOUBLE);
+    if (result == nullptr) return nullptr;
+    result->value = a * b;
+    
+    return result;
+}
+
+
+int initAxioms() {
+    Operation *addition = nullptr;
+    Operation *multiplication = nullptr;
+    
+    addition = malloc(sizeof(Operation));
+    if (addition == nullptr) goto error;
+    
+    addition->associative = 1;
+    addition->commutative = 1;
+    addition->symbol = '+';
+    addition->type = AXIOMATIC;
+    addition->definition = createOpFunc(add);
+    
+    Debug(0, "Binding addition operation\n");
+    if (!bindComponent(GLOBALCONTEXT->env, COMP_OPERATION, "+", addition)) goto error;
+    
+    multiplication = malloc(sizeof(Operation));
+    if (multiplication == nullptr) goto error;
+    
+    multiplication->associative = 1;
+    multiplication->commutative = 1;
+    multiplication->symbol = '*';
+    multiplication->type = AXIOMATIC;
+    multiplication->definition = createOpFunc(multiply);
+    
+    Debug(0, "Binding multiplication operation\n");
+    if (!bindComponent(GLOBALCONTEXT->env, COMP_OPERATION, "*", multiplication)) goto error;
+    
+    return 1;
+    
+    error:
+        perror("Error initializing axioms");
+        free(addition);
+        free(multiplication);
+        
+        return 0;
+}
